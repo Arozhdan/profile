dd8137fc4768f448f843f7bb777d27d3e11b3352
Arozhdan <halala30@gmail.com>
2020-01-13 23:55:23 +0100

    services done

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
 $(function () {
-  AOS.init()
+  AOS.init({
+    duration: 500,
+  })
+
+  $(".header-scroll").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".about").offset().top
+      },
+      'slow');
+  });
+
+  window.onscroll = function () {
+    myFunction()
+  };
+  const initialOffset = $("#sticky")[0].offsetTop;
+
+  function myFunction() {
+
+    if (window.pageYOffset > initialOffset) {
+      $("#sticky").first().addClass("fixed");
+    } else {
+      $("#sticky").first().removeClass("fixed");
+    }
+  }
 
 
 });
